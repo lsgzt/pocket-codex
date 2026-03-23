2f55f24fd7fd30b6db3891f25c94a66efda66ee2
Z User <z@container>
2026-03-23 14:20:40 +0000

    Add missing clip import to fix build error

--- PocketDev/app/src/main/java/com/pocketdev/app/ui/screens/EditorScreen.kt
+++ PocketDev/app/src/main/java/com/pocketdev/app/ui/screens/EditorScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.*
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.SpanStyle
